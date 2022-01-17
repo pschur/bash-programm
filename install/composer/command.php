3e8773d7350a9f6c716f3b9ad65072b02bbce11f
Paul Schur <paul.schur@gmx.de>
2022-01-17 22:46:44 +0000

    update

--- install/composer/command.php
+++ install/composer/command.php
@@ -4,9 +4,9 @@ define("SETUP_FILE", __DIR__.'/composer-setup.php');
 
 copy('https://getcomposer.org/installer', SETUP_FILE);
 if (hash_file('sha384', 'composer-setup.php') === '906a84df04cea2aa72f40b5f787e49f22d4c2f19492ac310e8cba5b96ac8b64115ac402c8cd292b8a03482574915d1a8') {
-	echo 'Installer verified';
+	echo "Installer verified \n";
 } else {
-	echo 'Installer corrupt'; unlink(SETUP_FILE);
+	echo 'Installer corrupt', "\n"; unlink(SETUP_FILE);
 	exit();
 }
 echo PHP_EOL;
